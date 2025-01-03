243152c11311e64143b89f8d3be809553f78dec6
vamsi525 <vamsi.qcs@gmail.com>
2025-01-03 22:47:46 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,7 +41,7 @@ if source_file and target_file:
         # Call Azure OpenAI API
         api_url = "https://azeupotoaipoc.openai.azure.com/openai/deployments/gpt-4o-2024-05-13/chat/completions"  # Replace with your endpoint
         headers = {
-            "Authorization": "Bearer f7ff57fb377745d6837df09affdbd97",  # Replace with your API key
+            "Authorization": f"Bearer f7ff57fb377745d6837df09affdbd97",  # Replace with your API key
             "Content-Type": "application/json"
         }
         data = {
