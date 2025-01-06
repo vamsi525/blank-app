8e64c34f41e6f9c25da045cfdbd9f72147806e7c
vamsi525 <vamsi.qcs@gmail.com>
2025-01-06 22:58:40 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -93,6 +93,14 @@ def generate_xslt(client, source_schema, target_schema):
             "role": "user",
             "content": f"Generate an XSLT that maps the following source schema to the target schema:\n\nSource Schema:\n{json.dumps(source_schema, indent=2)}\n\nTarget Schema:\n{json.dumps(target_schema, indent=2)}\nEnsure the XSLT is compatible with Oracle Integration Cloud (OIC) Gen 3 standards"
         },
+        {
+            "role": "user",
+            "content": f"Don't use template"
+        },
+        {
+            "role": "user",
+            "content": f"use namespace prefix"
+        },
           # {
           #    "role": "user",
           #    "content": f"Could you please generate an XSLT mapping that transforms the following source XML schema to the target XML schema? The transformation should be compatible with Oracle Integration Cloud (OIC) Gen 3 standards:\n\nSource Schema:\n{json.dumps(source_schema, indent=2)}\n\nTarget Schema:\n{json.dumps(target_schema, indent=2)}"
